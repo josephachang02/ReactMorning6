29d445cb5977d0abf759732ec71831086f129ffd
JoeChang <josephachang02@gmail.com>
2023-09-15 11:17:41 -0700

    fridya

---
+++ ReactMorning6.jsx
@@ -0,0 +1,13 @@
+//Create a react app. 
+
+// In App.js place this state array of houses. 
+
+ 
+
+//  const [houses, setHouses] = useState([ { name: 'Lakeside Villa', address: '123 Lake St, Watertown, MA', price: 1000000, isSold: false }, { name: 'Mountain Retreat', address: '456 Mountain Rd, Boulder, CO', price: 750000, isSold: true }, { name: 'Downtown Apartment', address: '789 Main St, Seattle, WA', price: 500000, isSold: false }]);
+// +
+// Make a page <Home /> in App.js
+
+// in home make a component <HouseDisplay />
+
+// in HouseDisplay, map through the houses array, and show each on in JSX.
